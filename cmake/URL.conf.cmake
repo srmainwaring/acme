9b582422d1fcaaedb159d9acf6007f64046ba9a8
Rhys Mainwaring <rhys.mainwaring@me.com>
2022-11-08 16:07:01 +0000

    macOS: update mathutils amd hermes urls to macOS forks
    Signed-off-by: Rhys Mainwaring <rhys.mainwaring@me.com>

--- cmake/URL.conf.cmake
+++ cmake/URL.conf.cmake
@@ -6,8 +6,10 @@ set(json_URL https://github.com/nlohmann/json.git)
 set(json_TAG v3.6.1 CACHE STRING "json version")
 
 # MathUtils
-set(mathutils_URL git@frydom-ce.org:ce/mathutils.git)
-set(mathutils_TAG v1.8 CACHE STRING "mathutils version")
+# set(mathutils_URL git@frydom-ce.org:ce/mathutils.git)
+# set(mathutils_TAG v1.8 CACHE STRING "mathutils version")
+set(mathutils_URL https://github.com/srmainwaring/mathutils.git)
+set(mathutils_TAG 4661e678c5905c6ce58d99886c2fbb6b7dd27882 CACHE STRING "mathutils version")
 set(MATHUTILS_BUILD_TESTS OFF CACHE BOOL "")
 set(MATHUTILS_BUILD_BOOST_TESTS OFF CACHE BOOL "")
 
@@ -16,5 +18,7 @@ set(googletest_URL https://github.com/google/googletest.git)
 set(googletest_TAG release-1.10.0 CACHE STRING "googletest version")
 
 # hermes
-set(hermes_URL git@frydom-ce.org:ce/hermes.git)
-set(hermes_TAG v1.4 CACHE STRING " version")
+# set(hermes_URL git@frydom-ce.org:ce/hermes.git)
+# set(hermes_TAG v1.4 CACHE STRING " version")
+set(hermes_URL https://github.com/srmainwaring/hermes.git)
+set(hermes_TAG a4c3cbf0bc2b01627bb550a59bf58737260d2140 CACHE STRING " version")
